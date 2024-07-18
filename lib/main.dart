db024f45da4bb35787d7d81e457f1d9f54b20040
yousefdewidar <ymahmoud1213@gmail.com>
2024-07-18 06:41:05 +0300

    add package for preview app in chrome

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:bookly/features/home/data/repos/home_repo_implement.dart';
 import 'package:bookly/features/home/presentation/view%20model/featured%20book%20cubit/featured_books_cubit.dart';
 import 'package:bookly/features/home/presentation/view%20model/newest%20books%20cubit/newest_books_cubit.dart';
 import 'package:bookly/features/home/presentation/views/home_view.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -11,7 +13,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:dio/dio.dart';
 
 void main() {
-  runApp(const Bookly());
+  runApp(DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => const Bookly(),
+  ));
 }
 
 class Bookly extends StatelessWidget {
